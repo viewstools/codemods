6e9441470cc1c3a4f7559c9b8c1bcf865c824893
Darío Javier Cravero <dario@uxtemple.com>
2018-02-12 23:49:49 +0000

    feat: use slot symbol in system scopes
    Upates `when hover` to `when <hover`

---
+++ codemod-use-slot-symbol-in-system-scopes.js
@@ -0,0 +1,34 @@
+const glob = require('fast-glob')
+const fs = require('fs')
+
+const isSystemScope = line =>
+  line === 'when hover' ||
+  line === 'when focus' ||
+  line === 'when placeholder' ||
+  line === 'when active' ||
+  line === 'when disabled'
+
+const updateSystemScope = line => `when <${line.split(' ')[1]}`
+
+function transform(rtext) {
+  const text = rtext.replace(/\r\n/g, '\n')
+  const lines = text.split('\n').map(line => line.trim())
+
+  const next = lines.map((line, i) => {
+    if (isSystemScope(line)) return updateSystemScope(line)
+
+    return line
+  })
+
+  return next.join('\n')
+}
+
+glob(['src/**/*.view'], {
+  bashNative: ['linux'],
+  cwd: __dirname,
+}).then(list => {
+  list.forEach(file => {
+    const content = fs.readFileSync(file, 'utf-8')
+    fs.writeFileSync(file, transform(content))
+  })
+})
