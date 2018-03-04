9eb374049f01f752078929c4671b46ffb2bd9f65
Darío Javier Cravero <dario@uxtemple.com>
2018-03-04 12:53:06 +0000

    fix: flex 1 to apply flexShrink as well as that's what the browser is defaulting to

---
+++ codemod-flex-to-flexGrow-and-flexShrink.js
@@ -0,0 +1,30 @@
+const glob = require('fast-glob')
+const fs = require('fs')
+
+function transform(rtext) {
+  const text = rtext.replace(/\r\n/g, '\n')
+  const lines = text.split('\n').map(line => line.trim())
+
+  const next = []
+
+  lines.forEach(line => {
+    if (line.startsWith('flex ')) {
+      next.push(line.replace('flex ', 'flexGrow '))
+      next.push('flexShrink 1')
+    } else {
+      next.push(line)
+    }
+  })
+
+  return next.join('\n')
+}
+
+glob(['src/**/*.view'], {
+  bashNative: ['linux'],
+  cwd: __dirname,
+}).then(list => {
+  list.forEach(file => {
+    const content = fs.readFileSync(file, 'utf-8')
+    fs.writeFileSync(file, transform(content))
+  })
+})
